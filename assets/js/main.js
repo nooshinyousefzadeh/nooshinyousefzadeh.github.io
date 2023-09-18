229c1a073fc8655a6ec84391988273d699608587
nooshinyousefzadeh <145292372+nooshinyousefzadeh@users.noreply.github.com>
2023-09-17 22:38:30 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,59 +231,6 @@ jQuery(document).ready(function($) {
         return false;
     });
 
-    /* ---------------------------------------------------------------------- */
-    /* ----------------------------- Portfolio ------------------------------ */
-    /* ---------------------------------------------------------------------- */
-
-
-    var filterList = {
-        init: function() {
-
-            // MixItUp plugin
-            // http://mixitup.io
-            $('#portfoliolist').mixitup({
-                targetSelector: '.portfolio',
-                filterSelector: '.filter',
-                effects: ['fade'],
-                easing: 'snap',
-                // call the hover effect
-                onMixEnd: filterList.hoverEffect()
-            });
-
-        },
-        hoverEffect: function() {
-
-            // Simple parallax effect
-            $('#portfoliolist .portfolio').hover(
-                    function() {
-                        $(this).find('.label').stop().animate({bottom: 0}, 200);
-                        $(this).find('img').stop().animate({top: -30}, 500);
-                    },
-                    function() {
-                        $(this).find('.label').stop().animate({bottom: -40}, 200);
-                        $(this).find('img').stop().animate({top: 0}, 300);
-                    }
-            );
-
-        }
-
-    };
-
-    // Run the show!
-    filterList.init();
-
-    /* ---------------------------------------------------------------------- */
-    /* ----------------------------- prettyPhoto ---------------------------- */
-    /* ---------------------------------------------------------------------- */
-
-    $("a[rel^='portfolio']").prettyPhoto({
-        animation_speed: 'fast', /* fast/slow/normal */
-        social_tools: '',
-        theme: 'pp_default',
-        horizontal_padding: 5,
-        deeplinking: false,
-    });
-
 
 
     /* ---------------------------------------------------------------------- */

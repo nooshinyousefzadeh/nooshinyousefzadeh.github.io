2e961d6a01e3bb4458a5f6933781634e2696cb1d
Nooshin Yousefzadeh <145292372+nooshinyousefzadeh@users.noreply.github.com>
2025-08-16 01:08:34 -0400

    Update send_email.php

--- _includes/send_email.php
+++ _includes/send_email.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
